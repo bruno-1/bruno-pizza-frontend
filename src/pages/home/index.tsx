81d09de89ff208c8c5bb546710e53aba494f535f
Bruno <bruno_benaglia2@outlook.com>
2022-01-30 08:53:32 -0300

    Organizing the code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,16 +3,18 @@ import "./style.css"
 function Home() {
   return (
     <div className="home">
-      <header className="header">
-        <h1 className="title"><span className="title__green">Bru</span>no Pi<span className="title__red">zza</span></h1>
-        <div className="basket">
+      <header className="header__home">
+        <h1 className="title__header"><span className="title__green">Bru</span>no Pi<span className="title__red">zza</span></h1>
+        <div className="basket__header">
           <i className="basket__icon fas fa-shopping-basket"></i>
           <span className="amount__basket">1</span>
         </div>
       </header>
-      <main className="main">
-        <h2 className="pizzaName">Seafood pizza</h2>
-        <div className="description"></div>
+      <main className="pizza">
+        <h2 className="name__pizza">Seafood pizza</h2>
+        <div className="description__pizza">
+          <div className="igredients__pizza"></div>
+        </div>
       </main>
     </div>
   )

e447ec8cba6b7fb0133d3a008189e907d7c3a44d
Bruno <bruno_benaglia2@outlook.com>
2022-01-30 18:43:19 -0300

    Added buttons and images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,7 +22,18 @@ function Home() {
             <span className="price__price">$18.00</span>
           </div>
         </section>
+        <section className="exchange__pizza">
+          <div className="button__exchange">
+            <button className="left__button">
+              <i className="icon__left far fa-arrow-alt-circle-left"></i>
+            </button>
+            <button className="right__button">
+              <i className="icon__right far fa-arrow-alt-circle-right"></i>
+            </button>
+          </div>
+        </section>
       </main>
+        <img className="img__pizza" src="https://png2.cleanpng.com/dy/3c2df8749ee049cc6e09ea9d2cebb011/L0KzQYm3UsI5N5p6j5H0aYP2gLBuTgBqgqtmRdt9YXzscbA0gCVqe5pzfZ9saHX1gsq0lP9uaaV0ReZqa3Wwf8b7TgBme6V0ReVuYXbyf7W0kPl7gpIyTdMENnWzdIK8VcZna2kzSakDM0e4RYW4VcE6QGQ8S6s8M0WxgLBu/kisspng-pizza-italian-cuisine-cherry-tomato-take-out-pesto-seafood-pizza-5a96e0d1556fc8.17837554151983739335.png" alt="Seafood pizza" />
     </div>
   )
 }

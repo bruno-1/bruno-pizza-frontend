2d911ac30332d2d4ef61e0f7622b01f6aba8608d
Bruno <bruno_benaglia2@outlook.com>
2022-03-02 22:55:53 -0300

    implementing login with google

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,11 +6,43 @@ import './style.css';
  * @return {JSX.Element}
 */
 function Login() {
+  // const [user, setUser] = useState(Object);
+
   const Logo = React.lazy(() => import('../../components/logo'));
+
+  /**
+   * Init auth2 google
+   * @return {gapi.auth2.GoogleAuth}
+   */
+  function googleAuthInit() {
+    return gapi.auth2.init({
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      scope: 'profile',
+    });
+  }
+
+  /**
+   * Login with google
+   * @return {void}
+   */
+  function loginWithGoogle() {
+    gapi.load('auth2', function() {
+      googleAuthInit().signIn();
+    });
+  }
+
   return (
-    <main className="main">
-      <Logo className="logo__main"/>
-    </main>
+    <>
+      <main className="main">
+        <Logo className="logo__main"/>
+        <button onClick={() => loginWithGoogle()} className="button__main">
+          <div className="divButton__button">
+            <img className="img__divButton" src="https://developers.google.com/identity/images/g-logo.png" />
+          </div>
+          <span className="txt_button">Fazer login com o Google</span>
+        </button>
+      </main>
+    </>
   );
 }
 
